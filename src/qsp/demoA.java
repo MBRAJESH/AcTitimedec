069341d793334030b6b47d0fb2963caaad9a141c
JUPITER <JUPITER@DESKTOP-87D2P7E>
2019-12-25 08:09:17 +0530

    second msg

---
+++ src/qsp/demoA.java
@@ -0,0 +1,15 @@
+package qsp;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class demoA 
+{
+	@Test
+	public void newtest()
+	{
+		Reporter.log("new test script...", true);
+	}
+	
+
+}
